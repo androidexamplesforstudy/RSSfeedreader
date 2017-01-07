76c432195c590ec1ff04fd0dc4865b2c5f8238c5
AdamK <Adamkubarek@gmail.com>
2017-01-07 16:09:30 +0100

    sketch mechanic

--- app/src/main/java/akubarek/pl/rssfeedreader/MainActivity.java
+++ app/src/main/java/akubarek/pl/rssfeedreader/MainActivity.java
@@ -1,13 +1,37 @@
 package akubarek.pl.rssfeedreader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d(TAG, "onCreate: starting AsyncTask");
+        DownloadData downloadData = new DownloadData();
+        downloadData.execute("URL");
+        Log.d(TAG, "onCreate: done");
+    }
+
+    private class DownloadData extends AsyncTask <String, Void, String> {
+        private static final String TAG = "DownloadData";
+        @Override
+        protected String doInBackground(String... params) {
+            Log.d(TAG, "doInBackground: starts with: " + params[0]);
+            return "doInBackground completed";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Log.d(TAG, "onPostExecute: parameter is" + s);
+        }
     }
 }
